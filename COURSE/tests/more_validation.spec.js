26675fa6613a992b604e148305736799b86edd7a
PedroAlves1500 <pedro.alves@tailwindsw.com>
2023-07-17 14:02:39 -0300

    MOD Changes on azure pipeline.

--- COURSE/tests/more_validation.spec.js
+++ COURSE/tests/more_validation.spec.js
@@ -9,6 +9,9 @@ test('Popup validations', async ({page}) => {
     const hide_element_button_css = page.locator('css=#hide-textbox');
     const show_element_button_css = page.locator('css=#show-textbox');
     const element_displayed_example_css = page.locator('css=#displayed-text');
+    const name_field_css = page.locator('css=#name');
+    const confirm_button_popup_css = page.locator('css=#confirmbtn');
+
 
     await (element_displayed_example_css).waitFor();
     //await expect (await (element_displayed_example_css).isVisible()).toEqual(true);
@@ -22,5 +25,13 @@ test('Popup validations', async ({page}) => {
 
     await expect(element_displayed_example_css).toBeVisible();
     //await expect (await (element_displayed_example_css).isVisible()).toEqual(true);
+
+    await (name_field_css).fill("TEST_NAME");
+    await (confirm_button_popup_css).click();
+    await (page.on('dialog',dialog => dialog.accept()));
+    
+
+
+
     
 })
